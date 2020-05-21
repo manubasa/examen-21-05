41b1ba68a0a7afd5458b74af58f9c806f289d2a4
manubasa <57366240+manubasa@users.noreply.github.com>
2020-05-21 16:12:50 +0200

    tercer ejercicio

--- 3.sh
+++ 3.sh
@@ -1,3 +1,9 @@
+totalventas=0
 for i in $( cat ventas_mensuales.txt | awk '{print$2}' ) ;do
 
 mes='cat ventas_mensuales.txt | awk'{print $2}''
+if [ $mes != "enero" | $mes != "septiembre" ]
+then
+totalventas= $(($totalventas+$i))
+fi
+echo $totalventas
